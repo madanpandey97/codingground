b39dab9fce16608e3686453e95e7e1b443ffe8e8
madan kumar pandey <madanpandey97@users.noreply.github.com>
2015-09-28 23:01:32 +0530

    Create fattripple.cpp

---
+++ fattripple.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int t;
+	long long k,num;
+	cin>>t;
+	while(t--)
+	{
+		cin>>k;
+		num = 192 + (k-1)*250;
+		cout<<num<<endl;
+		
+	}
+
+	return 0;
+}
